18b5731546422ab84bef17e9e6a94e20be1844a7
troy-fulton <troy@fulton5.com>
2020-03-15 04:38:44 +0000

    Add new integration to DynamoDB

--- service/mythicalMysfitsService.py
+++ service/mythicalMysfitsService.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, json, Response, request
 from flask_cors import CORS
+import mysfitsTableClient
 
 # A very basic API created using Flask that has two possible routes for requests.
 
@@ -12,23 +13,29 @@ CORS(app)
 def healthCheckResponse():
     return jsonify({"message" : "Nothing here, used for health check. Try /mysfits instead."})
 
-# The main API resource that the next version of the Mythical Mysfits website
-# will utilize. It returns the data for all of the Mysfits to be displayed on
-# the website.  Because we do not yet have any persistent storage available for
-# our application, the mysfits are simply stored in a static JSON file. Which is
-# read from the the filesystem, and directly used as the service response.
+# Returns the data for all of the Mysfits to be displayed on
+# the website.  If no filter query string is provided, all mysfits are retrived
+# and returned. If a querystring filter is provided, only those mysfits are queried.
 @app.route("/mysfits")
 def getMysfits():
 
-    # read the mysfits JSON from the listed file.
-    response = Response(open("mysfits-response.json").read())
+    filterCategory = request.args.get('filter')
+    if filterCategory:
+        filterValue = request.args.get('value')
+        queryParam = {
+            'filter': filterCategory,
+            'value': filterValue
+        }
+        # a filter query string was found, query only for those mysfits.
+        serviceResponse = mysfitsTableClient.queryMysfits(queryParam)
+    else:
+        # no filter was found, retrieve all mysfits.
+        serviceResponse = mysfitsTableClient.getAllMysfits()
 
-    # set the Content-Type header so that the browser is aware that the response
-    # is formatted as JSON and our frontend JavaScript code is able to
-    # appropriately parse the response.
-    response.headers["Content-Type"]= "application/json"
+    flaskResponse = Response(serviceResponse)
+    flaskResponse.headers["Content-Type"] = "application/json"
 
-    return response
+    return flaskResponse
 
 # Run the service on the local server it has been deployed to,
 # listening on port 8080.
